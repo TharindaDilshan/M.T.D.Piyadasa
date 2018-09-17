3a2a072450e9019d9b07e4a11d75fc15c43110a6
Tharinda Dilshan <43146932+TharindaDilshan@users.noreply.github.com>
2018-09-17 22:42:01 +0530

    added graph.c

---
+++ Graph.c
@@ -0,0 +1,149 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node_edge{
+	int data;
+	struct node_edge *nextedge;
+};
+struct node_vertex{
+	int data;
+	struct node_edge *link;
+	struct node_vertex *nextvertex;
+};
+struct node_vertex *head=NULL;
+
+struct node_vertex *create_v(struct node_vertex* ptr,int m){
+	if(ptr==NULL){
+//		printf("testing...");
+		struct node_vertex *newvertex;
+		newvertex=(struct node_vertex*)malloc(sizeof(struct node_vertex));
+		newvertex->data=m;
+		newvertex->nextvertex=NULL;
+		newvertex->link=NULL;
+		return newvertex;
+	}else{
+		ptr->nextvertex=create_v(ptr->nextvertex,m);
+	}
+	
+}
+struct node_edge *create_e(struct node_vertex *ptr,int x,int y){
+//	printf("wawawa");
+	while(ptr->data!=x){
+		ptr=ptr->nextvertex;
+	}
+	struct node_edge *edge=ptr->link;
+	if(edge!=NULL){
+//		printf("eyow");
+		while(edge->nextedge!=NULL){
+			edge=edge->nextedge;
+		}
+		struct node_edge *newedge;
+		newedge=(struct node_edge*)malloc(sizeof(struct node_edge));
+		newedge->data=y;
+		newedge->nextedge=NULL;
+		edge->nextedge=newedge;	
+	}else{
+//		printf("meowww");
+		struct node_edge *newedge;
+		newedge=(struct node_edge*)malloc(sizeof(struct node_edge));
+		newedge->data=y;
+		newedge->nextedge=NULL;
+		ptr->link=newedge;
+	}	
+}
+void print(struct node_vertex *ptr){
+	struct node_edge *edge;
+	printf("Vertex\t        Edge\n\n");
+	while(ptr!=NULL){
+		printf("%d\t\t",ptr->data);
+		edge=ptr->link;
+		ptr=ptr->nextvertex;
+		while(edge!=NULL){
+			printf("%d  ",edge->data);
+			edge=edge->nextedge;
+		}printf("\n");
+		printf("------------------------\n");
+		
+	}
+	
+}
+
+
+int main(){
+	int x,y,v,i,j,s,q;
+	while(1){
+		printf("1.Create Graph\n2.Display Graph\n3.Add New vertex\n4.Add New Edge\n5.Exit\n");
+		printf("Enter Selection: ");
+		scanf("%d",&s);
+	
+	
+		switch(s){
+			case 1:
+				printf("Enter the number of vertices: ");
+				scanf("%d",&v);
+				for(i=1;i<=v;i++){
+					head=create_v(head,i);
+				}
+				while(1){
+					printf("Enter edge v1 v2(enter 0 0 to end): ");
+					scanf("%d %d",&x,&y);
+					if(x==0||y==0){
+						break;
+					}else{
+						create_e(head,x,y);
+					}
+		
+				}
+				printf("Graph Created Successfully\n\n");
+				break;
+			case 2:
+				printf("\n\n");
+				print(head);
+				printf("\n\n");
+				break;
+			case 3:
+				printf("\nEnter Value: ");
+				scanf("%d",&q);
+				head=create_v(head,q);
+				break;
+			case 4:
+				while(1){
+					printf("Enter New Edge v1 v2(enter 0 0 to end): ");
+					scanf("%d %d",&x,&y);
+					if(x==0||y==0){
+						break;
+					}else{
+						create_e(head,x,y);
+					}
+		
+				}
+				break;
+				
+			case 5:
+				exit(0);
+		}
+	}
+	
+	
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
